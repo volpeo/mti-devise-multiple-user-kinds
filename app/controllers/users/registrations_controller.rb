5b5fde66d0b5358fe0ba743efa7f92be3c68c778
Sylvain	Peigney <sylvain@volpeo.fr>
2017-04-26 20:44:35 +0200

    User kind set up

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,28 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+
+    resource = params[:user][:user_kind].capitalize.constantize.new(sign_up_params)
+    resource.save
+
+    byebug
+
+    resource = resource.acting_as
+
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   actable
+
+  attr_reader :user_kind
 end
